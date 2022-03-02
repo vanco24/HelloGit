61f0638c4a91b0047dcc693f0753b40bf59c9058
wzx <945427827@qq.com>
2022-03-02 12:00:39 +0800

    使用HelloSpringboot小demo作为练习Git的项目.

---
+++ src/main/java/com/nole/HelloSpringBoot/HelloSpringBootApplication.java
@@ -0,0 +1,20 @@
+package com.nole.HelloSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController//@RestController 注释告诉 Spring，这段代码描述了一个应该在网络上可用的端点。
+public class HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value="name", defaultValue = "SpringBoot&Git") String name){
+		return String.format("Hello %s!", name);
+	}
+}
