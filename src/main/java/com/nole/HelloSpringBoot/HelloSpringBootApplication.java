30fbc93c52077ecc66bd8a2d91cb30d63bb55039
wzx <945427827@qq.com>
2022-03-02 12:25:59 +0800

    代码更改

--- src/main/java/com/nole/HelloSpringBoot/HelloSpringBootApplication.java
+++ src/main/java/com/nole/HelloSpringBoot/HelloSpringBootApplication.java
@@ -15,6 +15,6 @@ public class HelloSpringBootApplication {
 	}
 	@GetMapping("/helloGit")
 	public String hello(@RequestParam(value="name", defaultValue = "SpringBoot&Git") String name){
-		return String.format("Hello %s!", name);
+		return String.format("Hi %s!", name);
 	}
 }
